e7c80e0032fc8a100bb7dc7d98fc7f691a5ea4e2
Sara Bastian <seb118@georgetown.edu>
2020-09-25 02:55:37 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -10,7 +10,7 @@ def nyc_pigeon_organizer(data)
         new_hash[name] = {}
         
       end
-      if !new_hash[name][attribute] == nil
+      if new_hash[name][attribute] == inner_value
         new_hash[name][attribute] = []
       end
       new_hash[attribute].push()
