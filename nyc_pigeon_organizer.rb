8635731e64bfc6b364ca089f4548e1722bf1b366
Sara Bastian <seb118@georgetown.edu>
2020-09-25 01:59:03 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,25 @@
+require 'pry'
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  new_hash = {}
+  data.each do |attribute, value|
+    value.each do |inner_attribute, inner_value|
+      inner_value.each do |name|
+      if new_hash.include([name])
+        
+        binding.pry
+      end
+      new_hash
+    
+   
+  end
 end
+ 
+end
+
+#.each do |key, name|
+      #if data[:key].include?(name)
+        #new_array.push(name)
+        #elsif data[:key].each do |key, name|
+         # new_array.push(data[:key][:name])
+     
